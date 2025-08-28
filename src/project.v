6e8d527355b8f6281a6f1b9adacd30687397dd2a
tapeoutcutm <tapeout@cutm.ac.in>
2025-08-28 18:21:48 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_Improved_delay_PLL_1(
+module tt_um_Improved_delay_PLL(
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
