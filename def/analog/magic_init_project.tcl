2b394313e91456b2b65fe8ea24953adbb0d75631
tapeoutcutm <tapeout@cutm.ac.in>
2025-08-29 19:03:18 +0530

    Update magic_init_project.tcl

--- def/analog/magic_init_project.tcl
+++ def/analog/magic_init_project.tcl
@@ -8,7 +8,7 @@
 
 # Change the settings below to match your design:
 # ------------------------------------------------
-set TOP_LEVEL_CELL     tt_um_analog_example
+set TOP_LEVEL_CELL     tt_um_Improved_delay_PLL
 set TEMPLATE_FILE      tt_analog_1x2.def
 set POWER_STRIPE_WIDTH 2um                 ;# The minimum width is 1.2um
 
